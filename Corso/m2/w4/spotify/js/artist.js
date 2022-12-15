ba9c6156450ff329f6b8434b680183bacc26991e
Davide Piergiorgi <Piergio6294@gmail.com>
2022-12-15 19:45:51 +0100

    buildweek2vs1

--- Corso/m2/w4/spotify/js/artist.js
+++ Corso/m2/w4/spotify/js/artist.js
@@ -3,6 +3,116 @@ async function loadJSON(url) {
     return await res.json();
 }
 
-loadJSON('./json/album-json.JSON').then(data => {
+// variabili
+let lista = document.querySelector('ol')
+let i=1
+let nomeArtista = document.querySelector('#nome-artista');
+let fans = document.querySelector('#fans');
+let cantante = document.querySelector('#cantante');
+let sfondo = document.querySelector('#sfondo');
+let altriAlbum= document.getElementById('containerAlbum')
+const albums = [
+    "5327691",
+    "363906907",
+    "217489292",
+    "359324967",
+    "313482367",
+    "65373012",
+  ]
 
-})
+
+//funzione per calcolare il minutaggio
+function convertiInMinuti(durata) {
+    let minuti = Math.floor(durata / 60);
+    let secondi = durata - minuti * 60;
+    if(secondi < 10) {
+        return minuti + ":0" + secondi;
+    }
+    else {
+        return minuti + ":" + secondi;
+    }
+}
+
+
+// funzione per lista canzoni
+function listaSongs (data){
+    html=` <li class="row border-bottom border-secondary align-items-center">
+    <div class="col-1 numero d-none d-md-inline">${i}</div>
+    <div class="col-4 col-md-1 canzone">
+        <img src="${data.album.cover_small}" alt="imgAlbum" class="">
+    </div>
+    <div class="col-8 col-md-6 ">${data.title}</div>
+    <div class="col-2 views d-none d-md-block">${data.rank}</div>
+    <div class="col-2 durata d-none d-md-block">${convertiInMinuti(data.duration)}</div>
+</li>`
+lista.insertAdjacentHTML('beforeend', html)
+}
+
+
+// funzione per stampare album
+function renderAlbum(data){
+    const html = `<a href="../albumPage/album.html?albumId=${data.album.id}" class="text-decoration-none text-white col-md-2">
+    <div class="card">
+        <img src="${data.album.cover_medium}"
+            class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text">${data.album.title}</p>
+        </div>
+    </div>
+</a>`
+altriAlbum.insertAdjacentHTML('beforeend',html)
+document.getElementById('artista').textContent= `Discografia di ${data.artist.name}`
+}
+
+
+// funzione asincrona on load per caricare tutto
+window.onload = async () => {
+    let url = new URLSearchParams(window.location.search)
+    let id = url.get("artistId");
+    console.log(id)
+    loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}`).then(res => {
+        console.log(res);
+        nomeArtista.textContent = res.name;
+        fans.textContent = res.nb_fan+" Ascoltatori Mensili";
+        cantante.textContent = `di ${res.name}`;
+        sfondo.style.backgroundImage = `url('${res.picture_xl}')`
+        // richiamo funzioni per popolare html
+
+        
+
+    })
+    loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}/top?limit=50`).then(res=>{
+        console.log(res);
+        res.data.forEach(element => {
+            listaSongs(element);
+            i++;
+        });
+    })
+
+    
+    
+    //Load JSON per la sezione altri album dell'artista
+    loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}/top?limit=50`).then(res=>{
+        console.log(res);
+
+
+
+let indexes=[];
+        
+        // togliere doppioni album 
+        res.data.forEach(element => {
+            if(!indexes.includes(element.album.id))
+            {
+                renderAlbum(element);
+                indexes.push(element.album.id)
+            }
+            
+            
+        });
+    })
+
+
+    
+}
+
+
--- Corso/m2/w4/spotify/js/index.js
+++ Corso/m2/w4/spotify/js/index.js
@@ -1,70 +1,132 @@
-// variabili
 
+import { Album } from "../classi/Album.js";
+
+
+
+
+
+// variabili//////////////////////////////////////////////////////////
 let boxBuonasera = document.getElementById('box-articoli-buonasera')
 const albums = [
-    "5327691",
-    "363906907",
-    "217489292",
-    "359324967",
-    "313482367",
-    "65373012",
+  "5327691",
+  "363906907",
+  "9178393",
+  "359324967",
+  "9076739",
+  "65373012",
 ]
+let boxArtisti = document.querySelector("#artisti")
+const artists = [];
 
-
-
-async function loadJSON(url) {
-    const res = await fetch(url);
-    return await res.json();
+//funzione che crea numeri casuali//////////////////////////////////////////////
+for(let i = 0; i<5; i++) {
+  let num = Math.round(Math.random() * 10000);
+  artists[i] = num;
 }
 
-// funzione per creare elemento buonasera
-function articleBuonasera(data) {
-    const html = `
-    <a href="#" class="text-light text-decoration-none">
-    <div class="p-2 col-4">
-    <div class="card mb-3">
+ 
+
+// funzione per fare il json//////////////////////////////////////////////////
+async function loadJSON(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+
+// funzione per creare elemento buonasera////////////////////////////////////////
+function articleBuonasera(myAlbum) {
+  const html = `
+    <a  href="../albumPage/album.html?albumId=${myAlbum.id}" class="text-light text-decoration-none col-6 col-md-4" id="a">
+    <div class="p-2 " >
+    <div class="card">
       <div class="row g-0">
         <div class="col-md-4">
           <img
-            src="${data.cover_small}"
-            class="img-fluid rounded-start w-75" alt="...">
+            src="${myAlbum.img}"
+            class="img-fluid rounded-start w-100" alt="...">
         </div>
         <div class="col-md-8">
-          <div class="card-body mt-3">
-            <h5 class="card-title">${data.title}</h5>
-
+          <div class="card-body mt-2">
+            <h5 class="card-title">${myAlbum.title}</h5>
+            <p>${myAlbum.artista}</p>
           </div>
         </div>
       </div>
     </div>
   </div>
   </a>`
-    boxBuonasera.insertAdjacentHTML('beforeend', html)
+  boxBuonasera.insertAdjacentHTML('beforeend', html)
+}
+
+// funzione per creare html di ASCOLTA ANCHE/////////////////////////////////////////////
+function artistiHomepage(artisti) {
+  const card = ` <a   href="../artistPage/artisti.html?artistId=${artisti.id}"  class="text-decoration-none col-6 col-md-2  "> <div id="ultimo" class="text-light">
+  <div class="card">
+    <div class="carta">
+    <img
+      src="${artisti.picture_medium}"
+      class="card-img-top" alt="...">
+    </div>
+    <div class="card-body padding-testo d-flex">
+      <p class="card-text align-self-center">${artisti.name}</p>
+    </div>
+  </div>
+</div></a>`
+boxArtisti.insertAdjacentHTML('beforeend',card)
 }
 
 
+// fetch per l'header
+const id = 9076739;
+loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`).then(id => {
+  console.log(id);
+  let prova = document.querySelector(".prova");
+  prova.innerText = id.title;
+  let nome = document.querySelector("#nome");
+  let ascolta = document.querySelector('#ascolta');
+  let link = document.querySelector('#ancor');
+  link.setAttribute('href',`../artistPage/artisti.html?artistId=59`)
+  nome.innerHTML =`${id.artist.name}` ;
+  ascolta.innerHTML = `Ascolta l'album di ${id.artist.name}`
+  let prova1 = document.querySelector(".prova1");
+  prova1.setAttribute("src", id.cover_medium);
 
 
-loadJSON('../json/album-json.JSON').then(data => {
-    console.log(data);
-    let prova = document.querySelector(".prova");
-    prova.innerText = data.title;
-    let prova1 = document.querySelector(".prova1");
-    prova1.setAttribute("src", data.cover_medium);
-
-
-    articleBuonasera(data)
 })
 
 
-
+// funzione asincrona per stampare in BUONASERA////////////////////////////////////////////
 async function prova1(url) {
-    try {
-        loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/album/${url}`).then(data => {
-            console.log(data)
-            articleBuonasera(data)
-        })
-    } catch (err) { console.error(`${err}`) }
+  try {
+    loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/album/${url}`).then(data => {
+
+      console.log(data);
+      let myAlbum = new Album();
+      myAlbum.title = data.title;
+      myAlbum.img = data.cover_medium;
+      myAlbum.artista = data.artist.name;
+      myAlbum.id = data.id;
+      articleBuonasera(myAlbum)
+    })
+  } catch (err) { console.error(`${err}`) }
+}
+// richiamo funzione
+albums.forEach(ele => prova1(ele))
+
+
+
+// fdunzione asincrona per mostrare artisti random in ASCOLTA ANCHE/////////////////
+async function stampaArtista(id) {
+  try {
+    loadJSON(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}`).then(data_artist => {    
+      console.log(data_artist);
+      artistiHomepage(data_artist);
+      
+      })
+  } catch (err) { console.error(`${err}`) }
 }
 
-prova1(albums[3])
+// richiamo funzione nel ciclo
+for(let artista of artists) {
+  stampaArtista(artista);
+}
+
